459022d527c97d223c4cb7465f15060b5f92d740
smx_Moragn <1048492656@qq.com>
2024-10-11 18:22:47 +0800

    update

--- go.mod
+++ go.mod
@@ -1,9 +1,34 @@
 module github.com/kitex-contrib/monitor-prometheus
 
-go 1.13
+go 1.21
+
 
 require (
-	github.com/cloudwego/kitex v0.8.0
-	github.com/prometheus/client_golang v1.17.0
-	github.com/stretchr/testify v1.8.2
+	github.com/cloudwego/kitex v0.9.1
+	github.com/prometheus/client_golang v1.19.1
+	github.com/stretchr/testify v1.9.0
+	github.com/cloudwego-contrib/cwgo-pkg/telemetry v0.0.0
 )
+
+require (
+	github.com/apache/thrift v0.16.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bytedance/gopkg v0.0.0-20230728082804-614d0af6619b // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/google/pprof v0.0.0-20230509042627-b1315fad0c5a // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	golang.org/x/arch v0.5.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/cloudwego-contrib/cwgo-pkg/telemetry => D:\Projects\Go\cwgo-pkg\telemetry
--- tracer.go
+++ tracer.go
@@ -18,218 +18,56 @@
 package prometheus
 
 import (
-	"context"
-	"log"
-	"net/http"
-
-	prom "github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/collectors"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego-contrib/cwgo-pkg/telemetry/instrumentation/otelkitex"
+	"github.com/cloudwego-contrib/cwgo-pkg/telemetry/provider/promprovider"
 	"github.com/cloudwego/kitex/pkg/stats"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 )
-
-// Labels
-const (
-	labelKeyCaller = "caller"
-	labelKeyCallee = "callee"
-	labelKeyMethod = "method"
-	labelKeyStatus = "status"
-	labelKeyRetry  = "retry"
-
-	// status
-	statusSucceed = "succeed"
-	statusError   = "error"
-
-	unknownLabelValue = "unknown"
-)
-
-// genLabels make labels values.
-func genLabels(ri rpcinfo.RPCInfo) prom.Labels {
-	var (
-		labels = make(prom.Labels)
-
-		caller = ri.From()
-		callee = ri.To()
-	)
-	labels[labelKeyCaller] = defaultValIfEmpty(caller.ServiceName(), unknownLabelValue)
-	labels[labelKeyCallee] = defaultValIfEmpty(callee.ServiceName(), unknownLabelValue)
-	labels[labelKeyMethod] = defaultValIfEmpty(callee.Method(), unknownLabelValue)
-
-	labels[labelKeyStatus] = statusSucceed
-	if ri.Stats().Error() != nil {
-		labels[labelKeyStatus] = statusError
-	}
-
-	labels[labelKeyRetry] = "0"
-	if retriedCnt, ok := callee.Tag(rpcinfo.RetryTag); ok {
-		labels[labelKeyRetry] = retriedCnt
-	}
-
-	return labels
-}
-
-type clientTracer struct {
-	clientHandledCounter   *prom.CounterVec
-	clientHandledHistogram *prom.HistogramVec
-}
-
-// Start record the beginning of an RPC invocation.
-func (c *clientTracer) Start(ctx context.Context) context.Context {
-	return ctx
-}
-
-// Finish record after receiving the response of server.
-func (c *clientTracer) Finish(ctx context.Context) {
-	ri := rpcinfo.GetRPCInfo(ctx)
-	if ri.Stats().Level() == stats.LevelDisabled {
-		return
-	}
-	rpcStart := ri.Stats().GetEvent(stats.RPCStart)
-	rpcFinish := ri.Stats().GetEvent(stats.RPCFinish)
-	cost := rpcFinish.Time().Sub(rpcStart.Time())
-
-	extraLabels := make(prom.Labels)
-	extraLabels[labelKeyStatus] = statusSucceed
-	if ri.Stats().Error() != nil {
-		extraLabels[labelKeyStatus] = statusError
-	}
-
-	_ = counterAdd(c.clientHandledCounter, 1, genLabels(ri))
-	_ = histogramObserve(c.clientHandledHistogram, cost, genLabels(ri))
-}
 
 // NewClientTracer provide tracer for client call, addr and path is the scrape_configs for prometheus server.
 func NewClientTracer(addr, path string, options ...Option) stats.Tracer {
+
 	cfg := defaultConfig()
-	for _, opt := range options {
-		opt.apply(cfg)
+
+	for _, opts := range options {
+		opts.apply(cfg)
 	}
-
-	if !cfg.disableServer {
-		cfg.serveMux.Handle(path, promhttp.HandlerFor(cfg.registry, promhttp.HandlerOpts{
-			ErrorHandling: promhttp.ContinueOnError,
-			Registry:      cfg.registry,
-		}))
-		go func() {
-			if err := http.ListenAndServe(addr, cfg.serveMux); err != nil {
-				log.Fatal("Unable to start a promhttp server, err: " + err.Error())
-			}
-		}()
-	}
-
-	clientHandledCounter := prom.NewCounterVec(
-		prom.CounterOpts{
-			Name: "kitex_client_throughput",
-			Help: "Total number of RPCs completed by the client, regardless of success or failure.",
-		},
-		[]string{labelKeyCaller, labelKeyCallee, labelKeyMethod, labelKeyStatus, labelKeyRetry},
-	)
-	cfg.registry.MustRegister(clientHandledCounter)
-
-	clientHandledHistogram := prom.NewHistogramVec(
-		prom.HistogramOpts{
-			Name:    "kitex_client_latency_us",
-			Help:    "Latency (microseconds) of the RPC until it is finished.",
-			Buckets: cfg.buckets,
-		},
-		[]string{labelKeyCaller, labelKeyCallee, labelKeyMethod, labelKeyStatus, labelKeyRetry},
-	)
-	cfg.registry.MustRegister(clientHandledHistogram)
-
 	if cfg.enableGoCollector {
 		cfg.registry.MustRegister(collectors.NewGoCollector(collectors.WithGoCollectorRuntimeMetrics(cfg.runtimeMetricRules...)))
 	}
-
-	return &clientTracer{
-		clientHandledCounter:   clientHandledCounter,
-		clientHandledHistogram: clientHandledHistogram,
+	p := promprovider.NewPromProvider(
+		promprovider.WithRegistry(cfg.registry),
+		promprovider.WithHistogramBuckets(cfg.buckets),
+		promprovider.WithServiceName("server"),
+		promprovider.WithRPCServer(),
+	)
+	//prom provider not support serveMux
+	if !cfg.disableServer {
+		p.Serve(addr, path)
 	}
-}
-
-type serverTracer struct {
-	serverHandledCounter   *prom.CounterVec
-	serverHandledHistogram *prom.HistogramVec
-}
-
-// Start record the beginning of server handling request from client.
-func (c *serverTracer) Start(ctx context.Context) context.Context {
-	return ctx
-}
-
-// Finish record the ending of server handling request from client.
-func (c *serverTracer) Finish(ctx context.Context) {
-	ri := rpcinfo.GetRPCInfo(ctx)
-	if ri.Stats().Level() == stats.LevelDisabled {
-		return
-	}
-
-	rpcStart := ri.Stats().GetEvent(stats.RPCStart)
-	rpcFinish := ri.Stats().GetEvent(stats.RPCFinish)
-	cost := rpcFinish.Time().Sub(rpcStart.Time())
-
-	extraLabels := make(prom.Labels)
-	extraLabels[labelKeyStatus] = statusSucceed
-	if ri.Stats().Error() != nil {
-		extraLabels[labelKeyStatus] = statusError
-	}
-
-	_ = counterAdd(c.serverHandledCounter, 1, genLabels(ri))
-	_ = histogramObserve(c.serverHandledHistogram, cost, genLabels(ri))
+	return otelkitex.NewClientTracer()
 }
 
 // NewServerTracer provides tracer for server access, addr and path is the scrape_configs for prometheus server.
 func NewServerTracer(addr, path string, options ...Option) stats.Tracer {
 	cfg := defaultConfig()
-	for _, opt := range options {
-		opt.apply(cfg)
+
+	for _, opts := range options {
+		opts.apply(cfg)
 	}
-
-	if !cfg.disableServer {
-		cfg.serveMux.Handle(path, promhttp.HandlerFor(cfg.registry, promhttp.HandlerOpts{
-			ErrorHandling: promhttp.ContinueOnError,
-			Registry:      cfg.registry,
-		}))
-		go func() {
-			if err := http.ListenAndServe(addr, cfg.serveMux); err != nil {
-				log.Fatal("Unable to start a promhttp server, err: " + err.Error())
-			}
-		}()
-	}
-
-	serverHandledCounter := prom.NewCounterVec(
-		prom.CounterOpts{
-			Name: "kitex_server_throughput",
-			Help: "Total number of RPCs completed by the server, regardless of success or failure.",
-		},
-		[]string{labelKeyCaller, labelKeyCallee, labelKeyMethod, labelKeyStatus, labelKeyRetry},
-	)
-	cfg.registry.MustRegister(serverHandledCounter)
-
-	serverHandledHistogram := prom.NewHistogramVec(
-		prom.HistogramOpts{
-			Name:    "kitex_server_latency_us",
-			Help:    "Latency (microseconds) of RPC that had been application-level handled by the server.",
-			Buckets: cfg.buckets,
-		},
-		[]string{labelKeyCaller, labelKeyCallee, labelKeyMethod, labelKeyStatus, labelKeyRetry},
-	)
-	cfg.registry.MustRegister(serverHandledHistogram)
-
 	if cfg.enableGoCollector {
 		cfg.registry.MustRegister(collectors.NewGoCollector(collectors.WithGoCollectorRuntimeMetrics(cfg.runtimeMetricRules...)))
 	}
+	p := promprovider.NewPromProvider(
+		promprovider.WithRegistry(cfg.registry),
+		promprovider.WithHistogramBuckets(cfg.buckets),
+		promprovider.WithServiceName("server"),
+		promprovider.WithRPCServer(),
+	)
 
-	return &serverTracer{
-		serverHandledCounter:   serverHandledCounter,
-		serverHandledHistogram: serverHandledHistogram,
+	if !cfg.disableServer {
+		p.Serve(addr, path)
 	}
-}
 
-func defaultValIfEmpty(val, def string) string {
-	if val == "" {
-		return def
-	}
-	return val
+	return otelkitex.NewServerTracer()
 }
